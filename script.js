bfb8b75659c6abf9e83a0c728f8a2fe7de2cb5ef
Shanti Singh <110821342+shantisingh22@users.noreply.github.com>
2025-01-13 23:09:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
 const welcometrivia = document.getElementById('welcometrivia');
 const firstPlayerNameInput = document.getElementById('1stplayer1-name');
 const secondPlayerNameInput = document.getElementById('2ndplayer2-name');
 const submitButton = document.getElementById('SubmitButton');
 const textArea = document.getElementById("textArea");
 const textArea1 = document.getElementById("textArea1");
+const playerNamesSection = document.getElementById("player-names-section");
+const title1 = document.getElementById("title1");
 const categoryShow = document.querySelector(".catagory-show");
 const categoryDropdown = document.getElementById("categoryDropdown");
 const confirmCategoryButton = document.getElementById("confirmCategory");
@@ -15,50 +18,33 @@ submitButton.addEventListener("click", function () {
     const firstPlayerName = firstPlayerNameInput.value;
     const secondPlayerName = secondPlayerNameInput.value;
 
-    if (!firstPlayerName || !secondPlayerName) {
-        alert('Please enter both player names!');
-        return;
+    if (!firstPlayerName  &&  !secondPlayerName) {
+        console.log("Player 1:", firstPlayerName );
+        console.log("Player 2:", secondPlayerName);
     }
 
     welcometrivia.innerText = "Welcome To The Trivia Game";
-    welcometrivia.classList.remove('hidden');
+    welcometrivia.style.display = "block";
 
     textArea.innerText = `1st Player: ${firstPlayerName}`;
-    textArea.classList.remove('hidden');
-
+    textArea.style.display = "block";
+    
     textArea1.innerText = `2nd Player: ${secondPlayerName}`;
-    textArea1.classList.remove('hidden');
+    textArea1.style.display = "block";
+    
+    playerNamesSection.style.display = "none";
+    
+    firstPlayerNameInput.style.display = "none";
+    secondPlayerNameInput.style.display = "none";
+    submitButton.style.display = 'none';
 
-    firstPlayerNameInput.classList.add('hidden');
-    secondPlayerNameInput.classList.add('hidden');
-    submitButton.classList.add('hidden');
-
-    document.querySelector(".title1").style.display = 'none';
-    categoryShow.classList.remove("hidden");
+    document.querySelector(".title1").style.display = "none";
+    categoryShow.style.display = "block";
 });
 
 categoryDropdown.addEventListener("change", function () {
     confirmCategoryButton.classList.remove("hidden");
 });
-
-// confirmCategoryButton.addEventListener("click", function () {
-//     const selectedCategory = categoryDropdown.value;
-//     const difficulties = ["easy", "medium", "hard"];
-//     const Easy = "easy";
-//     if (selectedCategory) {
-//         fetch(`https://the-trivia-api.com/v2/questions?categories=${selectedCategory}&difficulties=${Easy}&limit=2`)
-
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data)
-//             })
-//             .catch(() => categoryShow.innerText = "Failed load data.");
-
-//             categoryShow.innerHTML = `${selectedCategory}`;
-//             confirmCategoryButton.classList.add("hidden");
-//     }
-// });
-
 
 confirmCategoryButton.addEventListener("click", function () {
     const selectedCategory = categoryDropdown.value;
@@ -100,7 +86,3 @@ confirmCategoryButton.addEventListener("click", function () {
         });
     }
 });
-
-
-
-
